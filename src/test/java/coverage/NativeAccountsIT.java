1d89e5a8af849e0ca556f8f00d4bcd4967720e72
Boosey <john.boudreaux@us.ibm.com>
2021-06-19 19:05:57 -0500

    Initial Commit

---
+++ src/test/java/coverage/NativeAccountsIT.java
@@ -0,0 +1,9 @@
+package coverage;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeAccountsIT extends AccountsTest {
+
+    // Execute the same tests but in native mode.
+}
